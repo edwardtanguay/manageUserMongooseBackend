4c3767ba3dcc6819820df00c583ea1b437290065
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-21 22:24:29 +0100

    before delete comments

--- backend.js
+++ backend.js
@@ -1,65 +1,91 @@
 import express from 'express';
-import mongodb, { MongoClient } from 'mongodb';
+// import mongodb, { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
 const port = 3016;
-const mongoConnectString = 'mongodb://localhost:27017';
-const client = new MongoClient(mongoConnectString);
+// const mongoConnectString = 'mongodb://localhost:27017';
+const mongoConnectString = 'mongodb://localhost:27017/api001';
+// const client = new MongoClient(mongoConnectString);
+mongoose.connect(mongoConnectString);
+
+const userSchema = mongoose.Schema({
+	name: String,
+	username: String,
+	email: String
+});
+const UserModel = mongoose.model("user", userSchema, "users100");
 
 app.use(cors());
 app.use(express.json());
 
-const execMongo = async (done) => {
-	await client.connect();
-	const db = client.db('api001');
-	done(db);
-}
+// const execMongo = async (done) => {
+// 	await client.connect();
+// 	const db = client.db('api001');
+// 	done(db);
+// }
 
-app.get('/', (req, res) => {
-	execMongo(async (db) => {
-		const users = await db.collection('users100').find()
-			.project({
-				name: 1,
-				username: 1,
-				email: 1
-			}).toArray();
-		res.json(users);
-	});
+app.get('/', async (req, res) => {
+	// execMongo(async (db) => {
+	// 	const users = await db.collection('users100').find()
+	// 		.project({
+	// 			name: 1,
+	// 			username: 1,
+	// 			email: 1
+	// 		}).toArray();
+	// 	res.json(users);
+	// });
+	const users = await UserModel.find({}).select('name username email');
+	res.json(users);
 });
 
-app.delete('/deleteuser/:id', (req, res) => {
+app.delete('/deleteuser/:id', async (req, res) => {
 	const id = req.params.id;
-	execMongo(async (db) => {
-		const deleteResult = await db.collection('users100').deleteOne({ _id: new mongodb.ObjectId(id) });
-		res.json({
-			result: deleteResult
-		});
-	});
+	// execMongo(async (db) => {
+	// 	const deleteResult = await db.collection('users100').deleteOne({ _id: new mongodb.ObjectId(id) });
+	// 	res.json({
+	// 		result: deleteResult
+	// 	});
+	// });
+	const deleteResult = await UserModel.deleteOne({ _id: new mongoose.Types.ObjectId(id) });
+	res.json({
+		result: deleteResult
+	})
 });
 
 app.post('/insertuser', (req, res) => {
 	const user = req.body.user;
-	execMongo(async (db) => {
-		const insertResult = await db.collection('users100').insertOne(user);
-		res.json({
-			result: insertResult 
-		});
+	// execMongo(async (db) => {
+	// 	const insertResult = await db.collection('users100').insertOne(user);
+	// 	res.json({
+	// 		result: insertResult
+	// 	});
+	// });
+	const user1 = new UserModel(user);
+	user1.save(err => {
+		if (err) {
+			res.status(500).send({ err })
+		} else {
+			res.json({
+				userAdded: user1
+			});
+		}
 	});
 });
 
-app.patch('/edituseremail/:id', (req, res) => {
+app.patch('/edituseremail/:id', async (req, res) => {
 	const id = req.params.id;
 	const email = req.body.email;
+	// execMongo(async (db) => {
+	// 	const updateResult = await db.collection('users100').updateOne({ _id: new mongodb.ObjectId(id) }, { $set: { email } });
+	// 	res.json({
+	// 		result: updateResult
+	// 	});
+	// });
+	const updateResult = await UserModel.findOneAndUpdate({ _id: new mongoose.Types.ObjectId(id) }, { $set: { email } }, { new: true });
 	res.json({
-		id,
-		email
-	});
-	execMongo(async (db) => {
-		const updateResult = await db.collection('users100').updateOne({ _id: new mongodb.ObjectId(id) }, { $set: { email } });
-		res.json({
-			result: updateResult 
-		});
+		result: updateResult
 	});
 });
 
